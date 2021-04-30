b9a55c488f22e213598d0cece06e24bcc6107eb4
jonathanoliveira.dev@gmail.com <jonathanoliveira.dev@gmail.com>
2021-04-30 00:06:47 -0300

    teste

---
+++ AngularJS/js/controllers.js
@@ -0,0 +1,22 @@
+angular.module("Nuvem3").controller("Nuvem3Ctrl", function($scope,$http,$filter){
+    $scope.valoresAprovados = [
+        {valorEntrada:80000,valorFinanciamento:11000,}
+    ];
+
+    $scope.bancos = [
+        { id: 1, nome: "Bradesco" },
+        { id: 2, nome: "Santander" },
+        { id: 3, nome: "Itau" },
+        { id: 4, nome: "Next" },
+        { id: 5, nome: "Nubank" }
+    ];
+
+    $scope.adicionarValores = function () {
+        $scope.valoresAprovados.push({});
+    };
+
+    $scope.apagarValores = function () {
+        $scope.valoresAprovados = []
+    };
+
+});
---
+++ AngularJS/js/filters/idadeFilter.js
@@ -0,0 +1,8 @@
+angular.module("Nuvem3").filter("idade",function($scope){
+    return function(id){
+
+        // console.log('ddd')
+        // console.log($scope.valoresAprovados)
+    return ;
+    };
+});
